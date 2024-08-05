084db2e756c932e20e233d3f8c1edb5a1ed0e990
t-ishitsuka <takuya.ishitsuka@gmail.com>
2024-08-05 14:12:07 +0900

    feat: gen command

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,4 +10,23 @@ app.use(
 );
 app.use(json());
 
+// TODO generate openapi yaml file
+// TODO generate openapi viewer html file
+// TODO add viewer openapi routing
+// TODO check openapi spec
+// TODO add response class
+// TODO add middleware (show req, res timestamp)
+// TODO add database
+// TODO add prisma setting
+// TODO gzip setting
+// TODO hogehoge.....
+// TODO 
+// TODO 
+// TODO 
+// TODO 
+// TODO 
+// TODO 
+// TODO 
+// TODO 
+
 RegisterRoutes(app);
---
+++ backend/src/openapi/config.ts
@@ -0,0 +1,30 @@
+import { ExtendedRoutesConfig, ExtendedSpecConfig } from 'tsoa';
+import { CompilerOptions } from 'typescript';
+
+export const specOptions: ExtendedSpecConfig = {
+  entryFile: "./src/server.ts",
+  noImplicitAdditionalProperties: 'throw-on-extras',
+  basePath: "./src",
+  specVersion: 3,
+  outputDirectory: "./src/openapi/build",
+  controllerPathGlobs: ["./src/http/controllers/api/**/*Controller.ts"],
+  specFileBaseName: 'openapi'
+};
+
+export const routeOptions: ExtendedRoutesConfig = {
+  entryFile: "./src/server.ts",
+  noImplicitAdditionalProperties: 'throw-on-extras',
+  basePath: "/api/v1",
+  routesDir: './src/routes',
+  bodyCoercion: true
+};
+
+export const compilerOptions: CompilerOptions = {
+  "baseUrl": "./",
+  "paths": {
+    "@/*": ["src/*"],
+    "/*": ["*"],
+    "tsoa": ["node_modules/tsoa/dist"],
+    "tsoa/": ["node_modules/tsoa/dist/"]
+  }
+}
